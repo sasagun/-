066af6f695590f9653b890dc6c202bebf3b4853b
sasagun <ifelsecasefor@163.com>
2017-06-13 17:52:53 +0800

    Create 爬一页动作片.py

--- 爬一页动作片.py
+++ 爬一页动作片.py
@@ -5,7 +5,7 @@ sys.setdefaultencoding("utf-8")
 from urllib import urlopen
 from collections import OrderedDict
 
-count = 1     #电影序号
+count = 1 		#电影序号
 
 def get_link_list(url):			#爬取一页中的正文链接
     text = urlopen(url).read().decode('gbk').encode('utf-8')
@@ -23,22 +23,22 @@ def get_one_page(target_links):			#进入到每个链接爬取电影信息
         try:
             item = re.search(patt,text)
             dict = OrderedDict([('片名',item.group(1)),
-							    ('产地',item.group(2)),
-							    ('类型',item.group(3)),
-							    ('大小',item.group(4)),
-							    ('片长',item.group(5)),
-							    ('下载',item.group(6))])
+	                        ('产地',item.group(2)),
+	                        ('类型',item.group(3)),
+	                        ('大小',item.group(4)),
+	                        ('片长',item.group(5)),
+	                        ('下载',item.group(6))])
         except AttributeError:
             print '>>>>>>>>>>>>>>>原网页第' + str(count) + '部信息有误<<<<<<<<<<<<<<<'
             count += 1
             continue
-        print str(count) + ('-'*240)      #用多个减号做分隔
+        print str(count) + ('-'*240) 		#用多个减号做分隔
         count += 1
         for i in dict.keys():
             print i, json.dumps(dict[i], encoding="UTF-8", ensure_ascii=False)
 			
 def main():
-    url = 'http://www.dy2018.com/2'			#动作片分类首页
+    url = 'http://www.dy2018.com/2' 		#动作片分类首页
     target_links = get_link_list(url)
     get_one_page(target_links)
 
